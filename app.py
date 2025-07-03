f043201be5c5bff26282abc8236e76950a22e6d4
mohamadanasfattoum <samaanas30@gmail.com>
2025-07-03 22:16:15 +0200

    initialize Flask app with SocketIO and create chat route

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def chat_home():
+    return render_template("chat.html")
+
+
+if __name__=="__main__":
+    socketio.run(app,debug=True)
